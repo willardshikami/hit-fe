123a6022e6d532499fae462c88a28822c70c23a2
willardshikami <willardshikami@gmail.com>
2017-10-07 04:59:34 +0300

    cors + bodyparser + clientside setup

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path')
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
@@ -9,17 +10,25 @@ const app = express();
 //port var
 const port = 3000;
 
+//linking to clientside
+app.use(express.static(path.join(__dirname, 'client')));
+
 //Connect to DB
 mongoose.connection.openUri('mongodb://localhost')
 const db = mongoose.connection;
 
+//cors middleware
+app.use(cors());
+
+//body parser middleware
+app.use(bodyParser.json());
 
 //API endpoint
 app.get('/', (req, res) => {
   res.send('API endpoint')
 });
 
-//listen to server
+//start server
 app.listen(port, () => {
   console.log('Server started on port ' + port);
 });
