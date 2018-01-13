0463b83d00aeb82b05230c5f5a492a2706a48064
willardshikami <willardshikami@gmail.com>
2018-01-13 22:16:10 +0300

    updated code to Angular 2

--- app/app.controller.js
+++
@@ -1,5 +0,0 @@
-angular.module('healthixSite')
-    healthixSite.controller('healthixController', ['$scope', '$http', '$location', '$routeParams', 'ModalService' ,function($scope, $http, $location, $routeParams, ModalService) {
-        
-    }]);
-    
