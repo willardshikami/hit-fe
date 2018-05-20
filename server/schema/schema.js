f1e0ea721c54b815b3ed32a1b66f9f85a5f07ec9
willardshikami <willardshikami@gmail.com>
2018-05-20 19:55:21 +0300

    added an new type

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,19 +1,38 @@
 const graphql = require('graphql');
 const { GraphQLObjectType,
         GraphQLString,
-        GraphQLSchema
+        GraphQLSchema,
+        GraphQLID
 
 }  = graphql;
 
 const TechieType= new GraphQLObjectType({
   name: 'Techie',
   fields: () => ({
-    id: {type: GraphQLString},
+    id: {type: GraphQLID},
     name: { type: GraphQLString },
     bio: { type: GraphQLString },
     img: { type: GraphQLString },
     stack: { type: GraphQLString },
-    position: { type: GraphQLString }
+    position: { type: GraphQLString },
+    link: {
+      type: LinkType,
+      resolve(parent, args){
+        return _.find(link, { id: parent.linkId})
+      }
+    }
+  })
+});
+
+const LinkType= new GraphQLObjectType({
+  name: 'Link',
+  fields: () => ({
+    id: {type: GraphQLID},
+    twitter: { type: GraphQLString },
+    instagram: { type: GraphQLString },
+    website: { type: GraphQLString },
+    linkedin: { type: GraphQLString },
+    github: { type: GraphQLString }
   })
 });
 
@@ -22,10 +41,17 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     techie: {
         type: TechieType,
-        args: { id: { type: GraphQLString }},
+        args: { id: { type: GraphQLID }},
         resolve(parent, args){
           //get data from db
         }
+    },
+    link: {
+      type: LinkType,
+      args: { id: { type: GraphQLID }},
+      resolve(parent, args){
+        //get data from db 
+      }
     }
   }
 });
