542295d881dc44c35dd0a177f018f7a82984032e
willardshikami <willardshikami@gmail.com>
2018-11-21 04:46:01 +0300

    configured routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'hit-fe';
+  title = 'Humans in Tech';
 }
---
+++ src/app/techie/techie.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-techie',
+  templateUrl: './techie.component.html',
+  styleUrls: ['./techie.component.css']
+})
+export class TechieComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
